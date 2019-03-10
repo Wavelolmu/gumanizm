9d1d43004e1e986df2f7ad4139482df9d616ef6a
Vitaly Gachkovsky <wavelolmu@gmail.com>
2019-03-10 08:44:57 +0300

    Add files via upload

---
+++ lgbt.js
@@ -0,0 +1,24 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  var arrow = scrolled + 80;
+  document.getElementById("myBar").style.width = scrolled + "%";
+};
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
